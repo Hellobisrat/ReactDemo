2dcc7648fa2816716a0798d4beab13e9d4fcb92d
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2025-08-26 16:46:32 -0500

    some modification

--- source/src/component/Navbar.jsx
+++ source/src/component/Navbar.jsx
@@ -9,7 +9,13 @@ const Navbar = ({isLoggedIn}) => {
   return (
     <div className='flex items-center justify-between md:p-2 lg:p-4 border-b  bg-rgba(255,255,255,0.8)'>
       <div className='md:text-medium lg:text-large flex gap:6'> <Link to='/'><HomeIcon/>| Home </Link>  </div>
-     <div>{ isLoggedIn ?  <span className='flex gap-4 '>Welcome, { <span className='font-semibold text-capital text-blue-800'>{name} </span> || 'Guest'}</span >: <Link to='/login'>Login</Link>}
+     <div className='flex gap-6 '>{ isLoggedIn ?  <span className='flex gap-4 '>Welcome, &nbsp; { <span className='font-semibold text-capital text-blue-800'>{name} </span>}</span >: <Link to='/login'>Login</Link>}
+     <Link
+      to="/login"
+     className={isLoggedIn ? 'block' : 'hidden'}
+      >
+     LogOut
+    </Link>
     </div> 
     </div>
   )
--- source/src/pages/Home.jsx
+++ source/src/pages/Home.jsx
@@ -14,8 +14,8 @@ const Home = () => {
   };
 
   const handleSearch = (e) => {
-    e.preventDefault(); // prevent form reload
-    setQuery(searchTerm); // update query only when button is clicked
+    e.preventDefault(); 
+    setQuery(searchTerm); 
   };
 
   const filteredProducts = products.filter(product =>
@@ -23,23 +23,24 @@ const Home = () => {
   );
 
 
-
   return (
     (loading  ? <section className='h-screen flex justify-center items-center'>
         Loading....
       </section> : <div className=' max-w-sm mx-auto md:max-w-none md:mx-0 p-2 md:p-4 lg:p-6 '>
         <div className='flex items-center justify-center border  border-slate-900 rounded-full w-[250px] lg:w-[700px] md:w-[400px] sm:w-[250px] my-6 md:my-10 lg:my-12 mx-auto relative'> 
-          <form onSubmit={handleSearch} className='flex justify-center w-full'>
-          
+          <form onSubmit={handleSearch} className='flex items-center w-full pl-4'>
+            <span>
+             <SearchIcon className='w-5 h-5'/>
+            </span>
             <input
              type='search'
              name='search'
              placeholder='Search your product'
              onChange={handleChange}
-             className='flex-1 px-8 p-2 rounded-full border-none'
+             className='w-full px-8 p-2 border border-gray-300 rounded-full outline-none'
              />
            
-             <button type='submit' className='bg-blue-400 text-white font-semibold mx-auto absolute right-0  top-0  rounded-r-full p-2 '>search</button>
+             <button type='submit' className='bg-blue-400 text-white font-semibold mx-auto rounded-r-full p-2  '>search</button>
           </form>
         </div>
         <div className='mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[30px] '>
@@ -61,4 +62,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default Home;
