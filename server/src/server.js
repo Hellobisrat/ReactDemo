5c17dd09ccc9951e19e8fefa039f4ba3c569b3b3
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2026-05-25 21:52:48 -0500

    last change

--- server/src/server.js
+++ server/src/server.js
@@ -32,5 +32,5 @@ app.use("/api/cart", cartRoutes);
 // Server start
 const PORT =process.env.PORT || 5000;
 app.listen(PORT,()=>{
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
 })
